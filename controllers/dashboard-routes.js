708653b06f00297fa4a5d477845d6a095e7db8e6
Aloha <utah.clayton@gmail.com>
2021-08-22 15:48:35 -0700

    completed routes, added style sheet, and minor updates to handlebars

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
+const { Post, User, Comment,} = require('../models');
 const withAuth = require('../utils/auth');
 
 // get all posts for dashboard
@@ -27,6 +27,7 @@ router.get('/', withAuth, (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
+      console.log(posts);
       res.render('dashboard', { posts, loggedIn: true });
     })
     .catch(err => {
